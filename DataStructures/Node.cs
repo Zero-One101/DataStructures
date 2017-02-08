dc66ac7f73faf7d1698c932e1151f1df9e36f05e
Stephen Badrick <stephenbadrick@gmail.com>
2017-02-08 13:10:43 +0000

    Added Node generic class and Node unit tests

---
+++ DataStructures/Node.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    /// <summary>
+    /// A node structure to serve as a container around a data object, holding references to parent and child nodes.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Parent { get; set; }
+        public Node<T> Child { get; set; }
+
+        /// <summary>
+        /// Constructs a new node with no parent or child
+        /// </summary>
+        /// <param name="data">The data contained by the node</param>
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        /// <summary>
+        /// Constructs a new node with a parent and child
+        /// </summary>
+        /// <param name="data">The dat acontained by the node</param>
+        /// <param name="parent">The parent to this node</param>
+        /// <param name="child">The child of this node</param>
+        public Node(T data, Node<T> parent, Node<T> child) : this(data)
+        {
+            Parent = parent;
+            Child = child;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Node<T> n = obj as Node<T>;
+            if((object)n == null)
+            {
+                return false;
+            }
+
+            return Data.Equals(n.Data);
+        }
+
+        public bool Equals(Node<T> n)
+        {
+            if ((object)n == null)
+            {
+                return false;
+            }
+
+            return Data.Equals(n.Data);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ DataStructuresTesting/NodeTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructuresTesting
+{
+    [TestClass]
+    public class NodeTests
+    {
+        [TestMethod]
+        public void NodeCorrectlyStoresData()
+        {
+            var node = new Node<string>("hello");
+            Assert.IsTrue(node.Data == "hello");
+        }
+
+        [TestMethod]
+        public void NodeCorrectlyStoresParent()
+        {
+            var parentNode = new Node<string>("hello");
+            var childNode = new Node<string>("world", parentNode, null);
+            Assert.IsTrue(childNode.Parent == parentNode);
+        }
+
+        [TestMethod]
+        public void NodeCorrectlyStoresChild()
+        {
+            var childNode = new Node<string>("world");
+            var parentNode = new Node<string>("hello", null, childNode);
+            Assert.IsTrue(parentNode.Child == childNode);
+        }
+    }
+}
